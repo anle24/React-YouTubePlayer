9990bb60b5d20e31a44dac1add117d906f17ea0a
Alan Le <alanle0824@gmail.com>
2018-01-31 17:38:33 -0800

    search function

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -4,17 +4,17 @@ const VideoDetail = ({video}) => {
   if (!video) {
     return <div>Loading...</div>
   }
-  
+
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
 
   return (
     <div className="video-detail col-md-8">
-      <div className="embed=responsive embed-responsive-16by9">
+      <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
       <div className="details">
-        <div>{video.snippet.title}</div>
+        <div><h5>{video.snippet.title}</h5></div>
         <div>{video.snippet.description}</div>
       </div>
     </div>
